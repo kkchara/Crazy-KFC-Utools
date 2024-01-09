43b5409b8cc90ba4b0c33cc47821050004f396a6
kkchara <80607663+kkchara@users.noreply.github.com>
2024-01-09 20:32:50 +0800

    1.0.3
    增加了复制按钮，刷屏更方便

--- preload.js
+++ preload.js
@@ -1,18 +1,7 @@
-utools.onPluginEnter(({ code, type, payload }) => {
-    console.log('用户进入插件应用', code, type, payload)
-})
+window.utoolPrase = function(text) {
+  utools.copyText(text)
+}
 
-utools.onPluginEnter(({ code, type, payload }) => {
-    console.log('用户进入插件应用', code, type, payload)
-})
-utools.onPluginOut((processExit) => {
-    if (processExit) {
-      console.log('插件应用完全退出')
-    } else {
-      console.log('插件应用隐藏后台')
-    }
-  })
-
-  utools.onPluginDetach(() => {
-    console.log('插件应用已作为系统窗口使用')
-  })
+window.api = {
+  utoolPrase
+}
